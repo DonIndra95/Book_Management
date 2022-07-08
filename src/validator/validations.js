404b5efcb925fbc68914708b2992841dadab1939
Kamaran Ahmed <ahmedkamaran893@gmail.com>
2022-07-08 12:23:06 +0530

    yunhi

--- src/validator/validations.js
+++ src/validator/validations.js
@@ -2,6 +2,28 @@
 const mongoose = require("mongoose");
 const ObjectId = mongoose.Types.ObjectId;
 const { isValidObjectId } = require("mongoose");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 const isValid = function (value) {
   if (typeof value == "undefined" || value == null) return false;
   if (typeof value == "string" && value.trim().length == 0) return false;
